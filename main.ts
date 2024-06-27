c92b532f4a752cd28a053378a675bd00cabae7bd
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-27 20:45:39 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,62 @@
+// Making Variables
+let Five = 5;
+let ten = 10;
+
+// Test 1
+console.log("Test 1: Five is equal to 5?");
+console.log(Five == 5);
+
+// Test 2
+console.log("Test 2: Ten is Equal to 10?");
+console.log(ten == 10);
+
+//Test 3
+console.log("Test 3: 5 is not equal to 10?");
+console.log(Five != ten);
+
+// Test 4
+console.log("Test 4: 10 is greater than 5?");
+console.log(ten > Five);
+
+// Test 5
+console.log("Test 5: five is smaller than 10?");
+console.log(Five<ten);
+
+// Test 6
+console.log("Test 6: 5 is equal to 10");
+console.log(Five == 10);
+
+//Test 7
+console.log("Test 7: 10 is smaller than 5?");
+console.log(ten < 5);
+
+//  Test 8
+console.log("Test 8: 10 is not equal to 10");
+console.log(ten != 10);
+
+// Test 9
+console.log("Test 9: 100 is smaller the 50?");
+console.log(100 < 50);
+
+// Test 10
+console.log("Test 10: five is greater tha 10?");
+console.log(Five > 10);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
